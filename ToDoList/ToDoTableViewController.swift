95d63acedadad94d74297c64d286da66b4dda3f1
mumu2plus <mumu2plus@gmail.com>
2018-12-18 11:40:43 +0800

    Add a new todo

---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by 加加林 on 2018/12/15.
+//  Copyright © 2018 mumu2plus. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    var todos = [ToDo]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let savedToDos = ToDo.loadToDos() {
+            todos = savedToDos
+        } else {
+            todos = ToDo.loadSampleToDos()
+        }
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier")
+            else { fatalError("Could not dequeue a cell") }
+        let todo = todos[indexPath.row]
+        cell.textLabel?.text = todo.title
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            todos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    @IBAction func unwindToToDoList(segue: UIStoryboardSegue) {
+        guard segue.identifier == "saveUnwind" else {return}
+        let sourceViewController = segue.source as! ToDoViewController
+        
+        if let todo = sourceViewController.todo {
+            let newIndexPath = IndexPath(row: todos.count, section: 0)
+            
+            todos.append(todo)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
+    }
+}
